ee58ea2aa9be460109bcbd8ee5a582c43684f59c
Kevin Keckeis <35276744+kevinkeckeis@users.noreply.github.com>
2022-06-21 21:56:42 +0200

    add routes to dashboard and login

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,34 @@
+import React, { useState } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 
+import {
+  DashboardContainer,
+  HeaderContainer,
+  LoginContainer,
+} from './containers';
+import Login from './containers/LoginContainer';
+
 function App() {
-  return <div className='App'>chck</div>;
+  const [token, setToken] = useState();
+
+  if (!token) {
+    return <Login setToken={setToken} />;
+  }
+
+  return (
+    <BrowserRouter>
+      <div className='app'>
+        <HeaderContainer />
+        <main>
+          <Routes>
+            <Route index element={<DashboardContainer />} />
+            <Route path='login' element={<LoginContainer />} />
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
